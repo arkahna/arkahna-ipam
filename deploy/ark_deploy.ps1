b72e142e6177d8bbb85e60147f55f4c34ab4da3d
Ryan Royals <ryan.royals@arkahna.io>
2024-05-24 14:31:51 +0930

    redeploy ipam

--- deploy/ark_deploy.ps1
+++ deploy/ark_deploy.ps1
@@ -6,6 +6,7 @@ $tags = @{
   'Environment'         = 'Production'
   'Owner'               = 'Cloud Platform'
   'deployedBy'          = 'Arkahna'  
+  'CreatedDate'         = '2024-05-24'
 }
 $resourcenames = @{
   appServiceName      = 'ark-app-prd-aue-ipam'
@@ -13,7 +14,7 @@ $resourcenames = @{
   cosmosAccountName   = 'ark-cosmos-prd-aue-ipam'
   cosmosContainerName = 'container01'
   cosmosDatabaseName  = 'database01'
-  keyVaultName        = 'ark-kv-prd-aue-ipam'
+  keyVaultName        = 'ark-kv-prd-aue-ipam01' # had to add number as the kv already existed and could not be purged
   workspaceName       = 'ark-log-prd-aue-ipam'
   managedIdentityName = 'ark-uai-prd-aue-ipam'
   resourceGroupName   = 'ark-rg-prd-aue-ipam'
